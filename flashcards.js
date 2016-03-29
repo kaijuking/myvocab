1c8fcf4edfdfff4ac1353250f2e9a51ad907d7da
Mike Field <field1062000@yahoo.com>
2016-03-29 16:26:05 -0700

    This commit fixes #2. I also added a new flashcards.js file which is house the individual cards for each deck.

---
+++ flashcards.js
@@ -0,0 +1,32 @@
+var flashcards = [
+  {
+    username: 'kaijuking',
+    deckname: 'JLPT N2 Verbs',
+    cards: [{word: 食べる, pronunciation: たべる, meaning: to eat, type: ru-verb},
+            {word: 食べる, pronunciation: たべる, meaning: to eat, type: ru-verb}]
+  },
+  {
+    username: 'kaijuking',
+    deckname: 'JLPT N2 Ajective',
+    cards: [{word: 食べる, pronunciation: たべる, meaning: to eat, type: ru-verb},
+            {word: 食べる, pronunciation: たべる, meaning: to eat, type: ru-verb}]
+  },
+  {
+    username: 'kaijuking',
+    deckname: 'Genki 1',
+    cards: [{word: 食べる, pronunciation: たべる, meaning: to eat, type: ru-verb},
+            {word: 食べる, pronunciation: たべる, meaning: to eat, type: ru-verb}]
+  },
+  {
+    username: 'kaijuking',
+    deckname: 'Genki 2',
+    cards: [{word: 食べる, pronunciation: たべる, meaning: to eat, type: ru-verb},
+            {word: 食べる, pronunciation: たべる, meaning: to eat, type: ru-verb}]
+  },
+  {
+    username: 'kaijuking',
+    deckname: 'Business Japanese',
+    cards: [{word: 食べる, pronunciation: たべる, meaning: to eat, type: ru-verb},
+            {word: 食べる, pronunciation: たべる, meaning: to eat, type: ru-verb}]
+  }
+]
